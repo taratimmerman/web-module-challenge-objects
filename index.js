639368b25cd314f44516594cf38ebdbef19549c2
taratimmerman <timmermantara@gmail.com>
2020-11-05 18:17:18 -0700

    5 passed, 5 total (MVP COMPLETE)

--- index.js
+++ index.js
@@ -139,12 +139,17 @@ Use the getLastReview function below to do the following:
   For example: getLastReview(reviews) would return: "Reyna gave the restaurant a 3.5 star review and, their feedback was: this place is chill with really cool people, great for getting work done on weekdays".
 */
 
-
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  let index = array.length - 1;
+  let lastReview = array[index];
+  let name = lastReview.name;
+  let rating = lastReview.rating;
+  let feedback = lastReview.feedback;
+  
+  return `${name} gave the restaurant a ${rating} star review, and their feedback was: ${feedback}`;
 } 
 
-
+console.log(getLastReview(reviews));
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
